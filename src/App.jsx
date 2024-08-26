a1b5ff630b84cbeb0a6bf8fcba28160c7be284e6
iitzIrFan <irfanulhawk@gmail.com>
2024-08-26 23:47:13 +0530

    Analytics...

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Box } from "@chakra-ui/react";
+import { Analytics } from '@vercel/analytics/react';
 import CodeEditor from "./components/CodeEditor";
+
 
 function App() {
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
       <CodeEditor />
+      <Analytics />
     </Box>
   );
 }
